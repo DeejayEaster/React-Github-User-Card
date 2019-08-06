c9cee2ede43e6ec132ca4c0d8833396eee4faaf3
deejayeaster <deejayeaster@live.com>
2019-08-06 17:43:20 -0400

    created UserData component

---
+++ user-card/src/components/UserData.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class UserData extends Component {
+    state = {  }
+    render() { 
+        return ();
+    }
+}
+ 
+export default UserData;
