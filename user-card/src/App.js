0cea5182ff1e16888756a0efb3bb4410a147220f
deejayeaster <deejayeaster@live.com>
2019-08-06 18:11:36 -0400

    got user data in axios call

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-
+import UserData from "./components/UserData";
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <UserData />
+    </div>
+  );
 }
 
 export default App;
--- user-card/src/components/UserData.js
+++ user-card/src/components/UserData.js
@@ -1,11 +1,39 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 class UserData extends Component {
-    state = {  }
-    render() { 
-        return ();
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      userData: null,
+      followersData: null
+    };
+  }
+
+  componentDidMount() {
+    const userData = axios
+      .get("https://api.github.com/users/deejayeaster", {})
+      .then(res => {
+        console.log("data", res.data);
+        this.setState({ userData: res.data });
+      })
+      .catch(err => {
+        console.log("error", err);
+      });
+
+    const userFollowers = axios
+      .get("https://api.github.com/users/deejayeaster/followers")
+      .then(res => {
+        console.log("follower data: ", res.data);
+        this.setState({ followersData: res.data });
+      })
+      .catch(err => console.log("follower error", err));
+  }
+
+  render() {
+    console.log("state userData", this.state.userData);
+    return <div />;
+  }
 }
- 
+
 export default UserData;
